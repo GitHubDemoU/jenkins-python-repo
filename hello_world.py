32f2c2df61181dd589a35f971c2114277adaf1ee
GitHubDemoU <119848894+GitHubDemoU@users.noreply.github.com>
2022-12-05 18:32:15 +0530

    Updated 2

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,3 @@
 #This is a python file to demonstrate the integration of this repo with Jenkins
 
-print('This repo will be integrated to Jenkins job in order to automate the building phase trial2')
+print('This repo will be integrated to Jenkins job in order to automate the building phase trial3')
